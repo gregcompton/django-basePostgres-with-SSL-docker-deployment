4f11adab8c292173c8648fa275b3feba56b25280
greg <greg@gregcompton.com>
2023-10-25 16:06:21 -0400

    attempt to fix csrf verification failure

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,3 +128,5 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["https://gregcompton.com", "https://www.gregcompton.com"]
